30bcd96e5436010bf9c4e0d6449704b0825bd866
kushal <gkushalg01@gmail.com>
2023-07-24 22:15:52 +0530

    Minor changes - still doesn't work

--- dummy-avoidance.py
+++ dummy-avoidance.py
@@ -2,7 +2,7 @@ import wx
 import random
 import math
 
-class Dot:
+class Bot:
     def __init__(self, position, radius, color, speed):
         self.position = position
         self.radius = radius
@@ -18,52 +18,52 @@ class Dot:
         self.velocity = (direction[0] * self.speed, direction[1] * self.speed)
         self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
 
-def repulsive_force(dot1, dot2, min_distance=15):
-    dx = dot2.position[0] - dot1.position[0]
-    dy = dot2.position[1] - dot1.position[1]
+def repulsive_force(Bot1, Bot2, min_distance=15):
+    dx = Bot2.position[0] - Bot1.position[0]
+    dy = Bot2.position[1] - Bot1.position[1]
     distance = math.hypot(dx, dy)
 
     if distance < min_distance:
         force = min_distance / distance
-        dot1.position = (dot1.position[0] - dx * force, dot1.position[1] - dy * force)
-        dot2.position = (dot2.position[0] + dx * force, dot2.position[1] + dy * force)
+        Bot1.position = (Bot1.position[0] - dx * force, Bot1.position[1] - dy * force)
+        Bot2.position = (Bot2.position[0] + dx * force, Bot2.position[1] + dy * force)
 
-class MyFrame(wx.Frame):
+class Model(wx.Frame):
     Timer_ID = 1
-    NumWhiteDots = 50
-    NumBlueDots = 5
+    NumWhiteBots = 50
+    NumBlueBots = 5
 
     def __init__(self, *args, **kw):
-        super(MyFrame, self).__init__(*args, **kw)
+        super(Model, self).__init__(*args, **kw)
 
         self.SetTitle("Collision Avoidance")
-        self.timer = wx.Timer(self, MyFrame.Timer_ID)
-        self.Bind(wx.EVT_TIMER, self.on_timer, id=MyFrame.Timer_ID)
-        self.red_dot = Dot((400, 300), 10, wx.Colour(255, 0, 0), 2.0)
-        self.white_dots = [Dot(self.get_random_point_inside_circle(400, 300, 100), 5, wx.Colour(255, 255, 255), 1.0) for _ in range(MyFrame.NumWhiteDots)]
-        self.blue_dots = [Dot(self.get_random_point_inside_circle(400, 300, 100), 5, wx.Colour(0, 0, 255), 3.0) for _ in range(MyFrame.NumBlueDots)]
+        self.timer = wx.Timer(self, Model.Timer_ID)
+        self.Bind(wx.EVT_TIMER, self.on_timer, id=Model.Timer_ID)
+        self.red_Bot = Bot((400, 300), 10, wx.Colour(255, 0, 0), 2.0)
+        self.white_Bots = [Bot(self.get_random_point_inside_circle(400, 300, 100), 5, wx.Colour(255, 255, 255), 1.0) for _ in range(Model.NumWhiteBots)]
+        self.blue_Bots = [Bot(self.get_random_point_inside_circle(400, 300, 100), 5, wx.Colour(0, 0, 255), 3.0) for _ in range(Model.NumBlueBots)]
         self.timer.Start(10)  # Update every 10 milliseconds
 
         self.Bind(wx.EVT_PAINT, self.on_paint)
 
     def on_timer(self, event):
-        self.move_dots()
+        self.move_Bots()
         self.Refresh()
 
-    def move_dots(self):
-        for dot in self.white_dots:
-            dot.move_towards(self.red_dot.position)
+    def move_Bots(self):
+        for Bot in self.white_Bots:
+            Bot.move_towards(self.red_Bot.position)
 
-        for dot in self.blue_dots:
-            dot.move_towards(self.red_dot.position)
+        for Bot in self.blue_Bots:
+            Bot.move_towards(self.red_Bot.position)
 
-        for i, dot1 in enumerate(self.white_dots):
-            for dot2 in self.white_dots[i+1:]:
-                repulsive_force(dot1, dot2)
+        for i, Bot1 in enumerate(self.white_Bots):
+            for Bot2 in self.white_Bots[i+1:]:
+                repulsive_force(Bot1, Bot2)
 
-        for dot1 in self.white_dots:
-            for dot2 in self.blue_dots:
-                repulsive_force(dot1, dot2, min_distance=25)
+        for Bot1 in self.white_Bots:
+            for Bot2 in self.blue_Bots:
+                repulsive_force(Bot1, Bot2, min_distance=25)
 
     def get_random_point_inside_circle(self, center_x, center_y, radius):
         while True:
@@ -74,25 +74,25 @@ class MyFrame(wx.Frame):
 
     def on_paint(self, event):
         dc = wx.BufferedPaintDC(self)
-        self.draw_dots(dc)
+        self.draw_Bots(dc)
 
-    def draw_dots(self, dc):
+    def draw_Bots(self, dc):
         dc.Clear()
 
-        for dot in self.white_dots:
-            dc.SetBrush(wx.Brush(dot.color))
-            dc.DrawCircle(int(dot.position[0]), int(dot.position[1]), dot.radius)
+        for Bot in self.white_Bots:
+            dc.SetBrush(wx.Brush(Bot.color))
+            dc.DrawCircle(int(Bot.position[0]), int(Bot.position[1]), Bot.radius)
 
-        for dot in self.blue_dots:
-            dc.SetBrush(wx.Brush(dot.color))
-            dc.DrawCircle(int(dot.position[0]), int(dot.position[1]), dot.radius)
+        for Bot in self.blue_Bots:
+            dc.SetBrush(wx.Brush(Bot.color))
+            dc.DrawCircle(int(Bot.position[0]), int(Bot.position[1]), Bot.radius)
 
-        dc.SetBrush(wx.Brush(self.red_dot.color))
-        dc.DrawCircle(int(self.red_dot.position[0]), int(self.red_dot.position[1]), self.red_dot.radius)
+        dc.SetBrush(wx.Brush(self.red_Bot.color))
+        dc.DrawCircle(int(self.red_Bot.position[0]), int(self.red_Bot.position[1]), self.red_Bot.radius)
 
 class MyApp(wx.App):
     def OnInit(self):
-        frame = MyFrame(None, wx.ID_ANY, "Collision Avoidance", size=(800, 600))
+        frame = Model(None, wx.ID_ANY, "Collision Avoidance", size=(800, 600))
         frame.Show(True)
         return True
 
